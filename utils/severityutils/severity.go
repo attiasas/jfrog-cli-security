aa25cc1cb78f66ed03fbda97d46e0bff51f37bb2
attiasas <assafattias93@gmail.com>
2025-04-04 23:44:18 +0300

    cdx severity score shows only 4 decimal digits

--- utils/severityutils/severity.go
+++ utils/severityutils/severity.go
@@ -256,7 +256,8 @@ func GetSeverityScore(severity Severity, applicabilityStatus jasutils.Applicabil
 
 func GetSeverityScoreFloat64(severity Severity, applicabilityStatus jasutils.ApplicabilityStatus) *float64 {
 	score := GetSeverityScore(severity, applicabilityStatus)
-	convertedScore := float64(score)
+	// convert score to have up to 4 digits after the decimal point
+	convertedScore := float64(int(score*10000)) / 10000
 	return &convertedScore
 }
 
